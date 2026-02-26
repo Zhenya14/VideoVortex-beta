ce9e540407115bce8ef3c745f905818e670012e9
Zhenya14 <118616573+Zhenya14@users.noreply.github.com>
2026-02-26 19:04:03 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,29 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+
+// --- Firebase config ---
+firebase.initializeApp({
+    apiKey: "AIzaSyBkPYP3bnDy61NFjRSboRZrfTVNTdIMWbY",
+  authDomain: "videovortex-235cd.firebaseapp.com",
+  databaseURL: "https://videovortex-235cd-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "videovortex-235cd",
+  storageBucket: "videovortex-235cd.appspot.com",
+  messagingSenderId: "681594250269",
+  appId: "1:681594250269:web:1176b21fcc8fe2a7d052f4"
+});
+
+const messaging = firebase.messaging();
+
+// --- Пуш-сповіщення ---
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[FCM] Background message received: ', payload);
+  const notificationTitle = payload.notification?.title || 'VideoVortex';
+  const notificationOptions = {
+    body: payload.notification?.body || '',
+    icon: 'VideoVortex_logo_192x192.png'
+  };
+  return self.registration.showNotification(notificationTitle, notificationOptions);
+});
 const CACHE_NAME = "videovortex-cache-v3";
 
 const ASSETS_TO_CACHE = [
